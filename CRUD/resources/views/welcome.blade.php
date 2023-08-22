c33b2599e798ef25af6f5b0450462e337f7762e7
Dairon <daairon252009@hormail.com>
2023-08-21 21:59:18 -0500

    Se crea la primera plantilla y parte de la primera view

---
+++ CRUD/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('theme.base')
+
+@section('content')
+
+    <div class="container py-5 text-center">
+      <h1 class="">Welcome</h1>
+      <a href="#" class="btn btn-primary">Clientes</a>
+
+    </div>
+@endsection
